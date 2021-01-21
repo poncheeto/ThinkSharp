6e1a0735af1d063c5c849b70a41ff7ea697112f3
Poncho <jplyles1212@gmail.com>
2021-01-20 20:18:49 -0800

    Modify to print output on separate new lines

--- exercise_2_2/exercise_2_2/Program.cs
+++ exercise_2_2/exercise_2_2/Program.cs
@@ -14,7 +14,10 @@ namespace exercise_2_2
             //Console.WriteLine(date);
             //Console.WriteLine(year);
 
-            Console.WriteLine($"European format: {day} {date} {month}, {year}");
+            Console.WriteLine($"American format:");
+            Console.WriteLine($"{day}, {month} {date}, {year}");
+            Console.WriteLine($"European format:");
+            Console.WriteLine($"{day} {date} {month}, {year}");
 
 
         }
