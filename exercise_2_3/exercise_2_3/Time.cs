abb427cd441b9d6e15bf424d7148809b8284d2ef
Poncho <jplyles1212@gmail.com>
2021-01-20 20:28:25 -0800

    Get total time in seconds

--- exercise_2_3/exercise_2_3/Time.cs
+++ exercise_2_3/exercise_2_3/Time.cs
@@ -6,7 +6,11 @@ namespace exercise_2_3
     {
         static void Main(string[] args)
         {
-            int hour = 8, minute = 22, second = 36;
+            int hour = 20, minute = 22, second = 36;
+
+            int sum_time = ((hour * 60) + minute) * 60;
+            Console.WriteLine(sum_time);
+            int seconds_since_midnight = 0;
         }
     }
 }
