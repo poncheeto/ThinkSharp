8bf66d18b3ec6b1969407f4cbf01808a57d68e41
Poncho <jplyles1212@gmail.com>
2021-01-20 20:21:43 -0800

    Create new project and rename program Time.cs

---
+++ exercise_2_3/exercise_2_3/Time.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exercise_2_3
+{
+    class Time
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
