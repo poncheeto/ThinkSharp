ae4df7de77b93a024493469f5d8b775c948bd912
Poncho <jplyles1212@gmail.com>
2021-01-20 19:47:43 -0800

    Move files into previous directory

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -3,11 +3,14 @@
 namespace ThinkSpace
 {
    public class Hello
-    {   
-        // main: generate some simple output
+    {
+        // main: generate some simple output;
         public static void Main(string[] args)
         {
+            ;
             Console.WriteLine("Hello, World.");
+            Console.WriteLine("How are you?");
+            // This is a comment!!!
         }
     }
 }
