5d06ccf2d3ce2a5fdfd6982f3f23b0e4f590095d
Poncho <jplyles1212@gmail.com>
2021-01-29 09:09:48 -0800

    Create CheckFermat method

--- Exercises/ConditionalsRecursion/ConditionalsRecursion/Program.cs
+++ Exercises/ConditionalsRecursion/ConditionalsRecursion/Program.cs
@@ -8,7 +8,9 @@ namespace ConditionalsRecursion
         {
             // Countdown(10);
             // NLines(12);
-            NinetyNineBottles(99);
+            // NinetyNineBottles(99);
+            CheckFermat(1, 2, 3, 4);
+            CheckFermat(3, 4, 5, 2);
         }
 
         //public static void Countdown(int n)
@@ -32,16 +34,33 @@ namespace ConditionalsRecursion
         //    }
         //}
 
-        static void NinetyNineBottles(int n)
+        // public static void NinetyNineBottles(int n)
+        //{
+        //    if (n == 0)
+        //    {
+        //        Console.WriteLine("No bottles of beer on the wall, no bottles of beer, ya’ can’t take one down, ya’can’t pass it around, ’cause there are no more bottles of beer on the wall!");
+        //    } else
+        //    {
+        //        Console.WriteLine($" {n} bottles of beer on the wall, {n} bottles of beer, ya’ take one down, ya’ pass it around, {n - 1} bottles of beer on the wall.");
+        //        NinetyNineBottles(n - 1);
+        //    }
+        //}
+
+        public static void CheckFermat(int a, int b, int c, int n)
         {
-            if (n == 0)
+
+            bool fermat = Math.Pow(a, n) + Math.Pow(b, n) == Math.Pow(c, n);
+
+            if (n > 2 && fermat) {
+                Console.WriteLine("Holy smokes, Fermat was wrong!");
+            } else if (n == 2 && fermat)
             {
-                Console.WriteLine("No bottles of beer on the wall, no bottles of beer, ya’ can’t take one down, ya’can’t pass it around, ’cause there are no more bottles of beer on the wall!");
+                Console.WriteLine("That works!");
             } else
             {
-                Console.WriteLine($" {n} bottles of beer on the wall, {n} bottles of beer, ya’ take one down, ya’ pass it around, {n - 1} bottles of beer on the wall.");
-                NinetyNineBottles(n - 1);
+                Console.WriteLine("No, that doesn't work.");
             }
+            Console.WriteLine(fermat);
         }
     }
 }
