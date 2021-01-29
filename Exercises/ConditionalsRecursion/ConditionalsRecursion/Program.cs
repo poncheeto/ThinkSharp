221bc4e1126b5afb44f3903d24df4a6b366102a6
Poncho <jplyles1212@gmail.com>
2021-01-29 08:37:21 -0800

    Create method NinetyNineBottles

--- Exercises/ConditionalsRecursion/ConditionalsRecursion/Program.cs
+++ Exercises/ConditionalsRecursion/ConditionalsRecursion/Program.cs
@@ -7,7 +7,8 @@ namespace ConditionalsRecursion
         static void Main(string[] args)
         {
             // Countdown(10);
-           // NLines(12);
+            // NLines(12);
+            NinetyNineBottles(99);
         }
 
         //public static void Countdown(int n)
@@ -31,6 +32,16 @@ namespace ConditionalsRecursion
         //    }
         //}
 
-
+        static void NinetyNineBottles(int n)
+        {
+            if (n == 0)
+            {
+                Console.WriteLine("No bottles of beer on the wall, no bottles of beer, ya’ can’t take one down, ya’can’t pass it around, ’cause there are no more bottles of beer on the wall!");
+            } else
+            {
+                Console.WriteLine($" {n} bottles of beer on the wall, {n} bottles of beer, ya’ take one down, ya’ pass it around, {n - 1} bottles of beer on the wall.");
+                NinetyNineBottles(n - 1);
+            }
+        }
     }
 }
