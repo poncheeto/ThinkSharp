b61d0258c0ba3cbc55c0b59a9a6e3165de2d5c49
Poncho <jplyles1212@gmail.com>
2021-02-03 14:18:08 -0800

    Create new Project for chapter 5 exercises

---
+++ Assignments/Week02Quiz01/Week02Quiz01/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSC205Quiz01
+{
+    class CSC205Quiz01
+    {
+        public static void Main(string[] args)
+        {
+            int dynamic = 0;
+            Console.WriteLine("#\n##\n ##\n  ##\n   #");
+        }
+    }  
+}
