68e7f8c63ff401ad9909cf33ffe7b99682b98b2e
Poncho <jplyles1212@gmail.com>
2021-02-03 14:27:35 -0800

    Call IsDivisible in Main to test

--- Exercises/ValueMethods/ValueMethods/Program.cs
+++ Exercises/ValueMethods/ValueMethods/Program.cs
@@ -6,13 +6,12 @@ namespace ValueMethods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(IsDivisible(4, 2));
         }
 
         public static bool IsDivisible(int n, int m)
         {
             return n % m == 0;
-           
         }
     }
 }
