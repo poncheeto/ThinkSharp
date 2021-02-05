009e91be12b06542a74939ed2a07584dc5fa876a
Poncho <jplyles1212@gmail.com>
2021-02-05 08:29:47 -0800

    Refactor IsTrianlge to one line of code

--- Exercises/ValueMethods/ValueMethods/Program.cs
+++ Exercises/ValueMethods/ValueMethods/Program.cs
@@ -8,17 +8,20 @@ namespace ValueMethods
         {
             // Console.WriteLine(IsDivisible(4, 2));
             // Console.WriteLine(Multadd(1.0, 2.0, 3.0));
+
+            //double a = Math.Cos(Math.PI / 4.0);
+            //double b = 1.0 / 2.0;
+            //double c = Math.Sin(Math.PI / 4.0);
+            //Console.WriteLine(Multadd(a, b, c));
+
+            //double a1 = 1.0;
+            //double b1 = Math.Log(10);
+            //double c1 = Math.Log(20);
+            //Console.WriteLine(Multadd(a1, b1, c1));
+
             // Yikes(12);
 
-            double a = Math.Cos(Math.PI / 4.0);
-            double b = 1.0 / 2.0;
-            double c = Math.Sin(Math.PI / 4.0);
-            Console.WriteLine(Multadd(a, b, c));
-
-            double a1 = 1.0;
-            double b1 = Math.Log(10);
-            double c1 = Math.Log(20);
-            Console.WriteLine(Multadd(a1, b1, c1));
+            Console.WriteLine(IsTriangle(1, 23, 3)); 
         }
 
         public static bool IsDivisible(int n, int m)
@@ -41,7 +44,7 @@ namespace ValueMethods
 
         public static bool IsTriangle(int a, int b, int c)
         {
-            return a + b >= c || a + c >= b || b + c >= a;
+            return !(a > b + c || b > a + c || c > a + b);
         }
     }
 }
