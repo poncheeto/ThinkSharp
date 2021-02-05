da84bdaf2d94e46e0bf08df5ba3d6af987f2de55
Poncho <jplyles1212@gmail.com>
2021-02-05 08:58:26 -0800

    Add Prod method to understand execution flow of recursive program

--- Exercises/ValueMethods/ValueMethods/Program.cs
+++ Exercises/ValueMethods/ValueMethods/Program.cs
@@ -4,47 +4,67 @@ namespace ValueMethods
 {
     class Program
     {
-        static void Main(string[] args)
+        //    static void Main(string[] args)
+        //    {
+        //        // Console.WriteLine(IsDivisible(4, 2));
+        //        // Console.WriteLine(Multadd(1.0, 2.0, 3.0));
+
+        //        //double a = Math.Cos(Math.PI / 4.0);
+        //        //double b = 1.0 / 2.0;
+        //        //double c = Math.Sin(Math.PI / 4.0);
+        //        //Console.WriteLine(Multadd(a, b, c));
+
+        //        //double a1 = 1.0;
+        //        //double b1 = Math.Log(10);
+        //        //double c1 = Math.Log(20);
+        //        //Console.WriteLine(Multadd(a1, b1, c1));
+
+        //        // Yikes(12);
+
+        //        Console.WriteLine(IsTriangle(1, 23, 3)); 
+        //    }
+
+        //    public static bool IsDivisible(int n, int m)
+        //    {
+        //        return n % m == 0;
+        //    }
+
+        //    public static double Multadd(double a, double b, double c)
+        //    {
+        //        return a * b + c;
+        //    }
+
+        //    public static double Yikes(double x)
+        //    {
+        //        double a = x;
+        //        double b = Math.Exp(-x);
+        //        double c = Math.Sqrt(1.0 - b);
+        //        return Multadd(a, b, c);
+        //    }
+
+        //    public static bool IsTriangle(int a, int b, int c)
+        //    {
+        //        return !(a > b + c || b > a + c || c > a + b);
+        //    }
+
+        public static void Main(String[] args)
         {
-            // Console.WriteLine(IsDivisible(4, 2));
-            // Console.WriteLine(Multadd(1.0, 2.0, 3.0));
-
-            //double a = Math.Cos(Math.PI / 4.0);
-            //double b = 1.0 / 2.0;
-            //double c = Math.Sin(Math.PI / 4.0);
-            //Console.WriteLine(Multadd(a, b, c));
-
-            //double a1 = 1.0;
-            //double b1 = Math.Log(10);
-            //double c1 = Math.Log(20);
-            //Console.WriteLine(Multadd(a1, b1, c1));
-
-            // Yikes(12);
-
-            Console.WriteLine(IsTriangle(1, 23, 3)); 
+            Console.WriteLine((Prod(1, 4)));
         }
 
-        public static bool IsDivisible(int n, int m)
+        public static int Prod(int m, int n)
         {
-            return n % m == 0;
+            if (m == n)
+            {
+                return n;
+            }
+            else
+            {
+                int recurse = Prod(m, n - 1);
+                int result = n * recurse;
+                return result;
+            }
         }
 
-        public static double Multadd(double a, double b, double c)
-        {
-            return a * b + c;
-        }
-
-        public static double Yikes(double x)
-        {
-            double a = x;
-            double b = Math.Exp(-x);
-            double c = Math.Sqrt(1.0 - b);
-            return Multadd(a, b, c);
-        }
-
-        public static bool IsTriangle(int a, int b, int c)
-        {
-            return !(a > b + c || b > a + c || c > a + b);
-        }
     }
 }
