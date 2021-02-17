f7d388384f6fb8823a15b13ba210b46ea3caab94
Poncho <jplyles1212@gmail.com>
2021-02-17 07:42:21 -0800

    Add new Objects Project for exercises

---
+++ Exercises/Objects/Objects/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
