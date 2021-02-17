a1b227f6497dd158934ed25419625f5f2a503039
Poncho <jplyles1212@gmail.com>
2021-02-17 07:52:50 -0800

    Create default Time constructor

--- Exercises/Objects/Objects/Program.cs
+++ Exercises/Objects/Objects/Program.cs
@@ -15,4 +15,11 @@ namespace Objects
         public int Hour, Minute;
         public double Second;
     }
+
+    public Time()
+    {
+        this.Hour = 0;
+        this.Minute = 0;
+        this.Second = 0.0;
+    }
 }
