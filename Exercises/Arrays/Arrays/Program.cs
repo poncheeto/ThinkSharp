efdddc03f7b9f2948f0dfe987e60d0bce93ea2b9
Poncho <jplyles1212@gmail.com>
2021-02-19 08:34:27 -0800

    Create method CloneArray

--- Exercises/Arrays/Arrays/Program.cs
+++ Exercises/Arrays/Arrays/Program.cs
@@ -6,12 +6,30 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            Random random = new Random();
-            for (int i = 0; i < 10; i++)
-            {
-                double x = random.NextDouble();
-                Console.WriteLine(x);
-            }
+            //Random random = new Random();
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    double x = random.NextDouble();
+            //    Console.WriteLine(x);
+            //}
+
+            int[] array = new int[5] { 1, 2, 3, 4, 5 };
+            Console.WriteLine(CloneArray(array));
         }
+
+        static public int[] CloneArray(int[] array)
+        {
+            int size = array.Length;
+            int[] newArray = new int[size];
+
+            int i = 0;
+            while (i < size)
+            {
+                newArray[i] = array[i];
+                i++;
+            }
+            return newArray;
+        }
+
     }
 }
